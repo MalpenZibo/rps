bd3a32f9a42040f595c55792af198d1a8e4c3f2f
Zibo Camito <zibo.camito@gmail.com>
2020-03-25 15:22:55 +0100

    more readable play fn

--- src/main/scala/rps/Game.scala
+++ src/main/scala/rps/Game.scala
@@ -10,16 +10,17 @@ import rps.models.Move._
 object Game {
   def play(userMove: Move): (Move, Move, Result) = {
     val enemyMove = getRandomMove
-    (
-      userMove, 
-      enemyMove, 
-      (userMove, enemyMove) match {
-        case (x, y) if x == y => Draw
-        case (Rock, Scissors) | (Paper, Rock) | (Scissors, Paper) => Win
-        case _ => Lose
-      }
-    )
+    val result = getResult(userMove, enemyMove)
+
+    (userMove, enemyMove, result)
   }
+
+  private def getResult(userMove: Move, enemyMove: Move) = 
+    (userMove, enemyMove) match {
+      case (x, y) if x == y => Draw
+      case (Rock, Scissors) | (Paper, Rock) | (Scissors, Paper) => Win
+      case _ => Lose
+    }
 
   private def getRandomMove(): Move =
     Random.shuffle(List(Rock, Paper, Scissors)).head
