cd9b3e19a680b60564cdd8639e9447f10c72ce13
Simone Camito <simone.camito@buildo.io>
2020-04-27 10:51:48 +0200

    use of dockercomposehost module
    removed unused output

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,84 +1,34 @@
-locals {
-  buildo_ip = "151.0.219.218/32"
+module "dockercomposehost" {
+  source  = "buildo/dockercomposehost/aws"
+  version = "1.4.1"
+
+  zone_id      = "${data.aws_route53_zone.simonerps-our-buildo-io.zone_id}"
+  ssh_key_name = "aws-simonerps"
+
+  project_name        = "simonerps"
+  host_name           = "simonerps.our.buildo.io"
+  ssh_private_key     = "${var.ssh_private_key}"
+  quay_password       = "${var.quay_password}"
+  in_open_ports       = ["80"]
+  in_cidr_blocks      = ["${local.buildo_ip}"]
+  bellosguardo_target = "buildo"
 }
 
-variable "access_key_value" {}
+data "aws_route53_zone" "simonerps-our-buildo-io" {
+  name = "simonerps.our.buildo.io"
+}
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
+variable quay_password {}
 
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-*-18.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
+variable ssh_private_key {
+  default = "~/.ssh/rps"
 }
 
 resource "aws_key_pair" "access" {
   key_name   = "access-key"
-  public_key = "${var.access_key_value}"
+  public_key = "${file("rps.pub")}"
 }
 
-resource "aws_instance" "rps" {
-  ami           = "${data.aws_ami.ubuntu.id}"
-  instance_type = "t3a.nano"
-
-  vpc_security_group_ids = ["${aws_security_group.sg.id}"]
-
-  root_block_device {
-    volume_type = "gp2"
-    volume_size = 10
-  }
-
-  key_name = "${aws_key_pair.access.key_name}"
-}
-
-resource "aws_security_group" "sg" {
-  name = "simonerps"
-}
-
-resource "aws_security_group_rule" "ssh_in" {
-  type              = "ingress"
-  protocol          = "tcp"
-  security_group_id = "${aws_security_group.sg.id}"
-  from_port         = 22
-  to_port           = 22
-  cidr_blocks       = ["${local.buildo_ip}"]
-}
-
-resource "aws_security_group_rule" "web_in" {
-  type              = "ingress"
-  protocol          = "tcp"
-  security_group_id = "${aws_security_group.sg.id}"
-  from_port         = 80
-  to_port           = 80
-  cidr_blocks       = ["${local.buildo_ip}"]
-}
-
-
-resource "aws_security_group_rule" "all_out" {
-  type              = "egress"
-  protocol          = -1
-  security_group_id = "${aws_security_group.sg.id}"
-  from_port         = 0
-  to_port           = 0
-  cidr_blocks       = ["0.0.0.0/0"]
-}
-
-data "aws_route53_zone" "buildo-io" {
-  name = "buildo.io"
-}
-
-resource "aws_route53_record" "simonerps-our-buildo-io-A" {
-  zone_id = "${data.aws_route53_zone.buildo-io.id}"
-  name    = "simonerps.our.buildo.io"
-  type    = "A"
-  records = ["${aws_instance.rps.public_ip}"]
-  ttl     = "1800"
+locals {
+  buildo_ip = "151.0.219.218/32"
 }
